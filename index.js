d490f5f87c7dcddea3184e0a80dde7e923be44fd
Anurag Bansal <devanuragbansal@gmail.com>
2024-12-26 17:27:57 +0530

    erase functionality added

--- index.js
+++ index.js
@@ -50,33 +50,46 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-document.addEventListener("resize", () => {
+let canvasData;
+
+window.addEventListener("resize", () => {
+    canvasData = ctx.getImageData(0, 0, canvas.width, canvas.height);
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-})
+    if (canvasData) ctx.putImageData(canvasData, 0, 0);
+});
+
 
 let isDrawing = false;
+let isErasing = false
 
 canvas.addEventListener('mousedown', (e) => {
   if (drawingOption === "pencil") { 
     isDrawing = true;
     ctx.beginPath();
     drawLine(e.clientX, e.clientY);
+  }else if(drawingOption === "eraser"){
+    isErasing = true
+    eraseLine(e.clientX, e.clientY)
   }
 });
 
 canvas.addEventListener('mousemove', (e) => {
   if (isDrawing && drawingOption === "pencil") { 
     drawLine(e.clientX, e.clientY);
+  }else if(isErasing && drawingOption === "eraser"){
+    eraseLine(e.clientX, e.clientY)
   }
 });
 
 canvas.addEventListener('mouseup', () => {
   isDrawing = false;
+  isErasing = false
 });
 
 canvas.addEventListener('mouseleave', () => {
   isDrawing = false;
+  isErasing = false
 });
 
 function drawLine(x, y) {
@@ -88,4 +101,13 @@ function drawLine(x, y) {
   ctx.stroke();
   ctx.beginPath();
   ctx.moveTo(x, y);
+}
+
+function eraseLine(x, y) {
+  ctx.save();
+  ctx.globalCompositeOperation = 'destination-out';
+  ctx.beginPath();
+  ctx.arc(x, y, 20, 0, 2 * Math.PI);
+  ctx.fill();
+  ctx.restore();
 }

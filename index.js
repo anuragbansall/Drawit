aed723eaff1bafe0490de8fc7538f2bf6668b6a0
Anurag Bansal <devanuragbansal@gmail.com>
2024-12-26 14:55:31 +0530

    draw function added

--- index.js
+++ index.js
@@ -4,26 +4,32 @@ const drawingSizes = document.getElementById('drawing-sizes');
 
 let selectedColor = "000000";
 let drawingOption = "";
-let drawingSize = "1";
+let drawingSize = 1;
 
 colorOptions.addEventListener('click', (e) => {
-  if (e.target.classList.contains('color-option')) {
-    selectedColor = e.target.dataset.colorHex;
-    console.log(`Selected color: ${selectedColor}`);
-  }
-});
-
-let previousDrawingOption = null;
+    if (e.target.classList.contains('color-option')) {
+      document.querySelectorAll('.color-option.active').forEach((option) => {
+        option.classList.remove('active');
+      });
+  
+      const selectedColorElement = e.target;
+      selectedColorElement.classList.add('active');
+  
+      selectedColor = selectedColorElement.dataset.colorHex;
+      console.log(`Selected color: ${selectedColor}`);
+    }
+  });
+  
 
 drawingOptions.addEventListener('click', (e) => {
   const drawingOptionElement = e.target.closest('.drawing-option');
   
   if (drawingOptionElement) {
-    if (previousDrawingOption && previousDrawingOption !== drawingOptionElement) {
-      previousDrawingOption.style.scale = 1;
-    }
+    document.querySelectorAll('.drawing-option.active').forEach((option) => {
+        option.classList.remove('active');
+      });
 
-    drawingOptionElement.style.scale = 1.4;
+    drawingOptionElement.classList.add('active');
     drawingOption = drawingOptionElement.dataset.tool;
     console.log(`Selected option: ${drawingOption}`);
 
@@ -35,3 +41,51 @@ drawingSizes.addEventListener('change', (e) => {
   drawingSize = e.target.value;
   console.log(`Selected size: ${drawingSize}`);
 });
+
+
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+document.addEventListener("resize", () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+})
+
+let isDrawing = false;
+
+canvas.addEventListener('mousedown', (e) => {
+  if (drawingOption === "pencil") { 
+    isDrawing = true;
+    ctx.beginPath();
+    drawLine(e.clientX, e.clientY);
+  }
+});
+
+canvas.addEventListener('mousemove', (e) => {
+  if (isDrawing && drawingOption === "pencil") { 
+    drawLine(e.clientX, e.clientY);
+  }
+});
+
+canvas.addEventListener('mouseup', () => {
+  isDrawing = false;
+});
+
+canvas.addEventListener('mouseleave', () => {
+  isDrawing = false;
+});
+
+function drawLine(x, y) {
+  ctx.lineTo(x, y);
+  ctx.strokeStyle = `${selectedColor}`;
+  ctx.lineWidth = drawingSize;
+  ctx.lineCap = 'round';
+  ctx.lineJoin = 'round';
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+}
